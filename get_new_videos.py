96a1e45ce1b27f0e37c33b2c2bfcf3658b6272b8
mirror <mirror12k@gmail.com>
2016-03-29 02:22:15 -0400

    - implemented a subscriptions file to ease listing of subscribers

--- get_new_videos.py
+++ get_new_videos.py
@@ -7,7 +7,7 @@ import sys
 
 
 def parseTime(time):
-	match = re.match(r'^(\d+) (second|minute|hour|day|week|month|year)s? ago$', time)
+	match = re.match(r'^(\d+) (second|minute|hour|day|week|month|year)s?(?: ago)?$', time)
 	if match is None:
 		raise Exception("invalid time given: '"+time+'"')
 	else:
@@ -31,20 +31,27 @@ def parseTime(time):
 def getRecent(channel, time):
 	timeFrame = parseTime(time)
 
-	return [ video for video in youtube_api.channelVideos(channel) if parseTime(video['age']) <= timeFrame ]
+	return [ video for video in youtube_api.channelVideos(channel+'/videos') if parseTime(video['age']) <= timeFrame ]
 
 
 
 
 
 if __name__ == '__main__':
-	time = sys.argv[1]
-	for channel in [
-		'user/TeamFortressTV/videos',
-	]:
-		videos = getRecent(channel, time)
-		if len(videos) > 0:
-			print('[['+channel+']]:')
-			for video in videos:
-				print('\t"'+video['title']+'" -', video['link'], '- (', video['age'], ')')
+	if len(sys.argv) < 2:
+		print("time frame argument required (ex. '1 day')")
+	else:
+		time = ' '.join(sys.argv[1:])
+		with open('subs.lst') as f:
+			# iterate the file
+			for channel in f:
+				channel = channel.strip()
+				# ensure we have a channel on this line
+				if channel != '' and channel[0] != '#':
+					videos = getRecent(channel, time)
+					# if there are videos, print a nice header for it
+					if len(videos) > 0:
+						print('[['+channel+']]:')
+						for video in videos:
+							print('\t"'+video['title']+'" -', video['link'], '- (', video['age'], ')')
 
--- youtube_api.py
+++ youtube_api.py
@@ -14,12 +14,11 @@ def channelVideos(channel):
 		videoLinkElement = videoItem.xpath('.//*[contains(@class, "yt-lockup-title")]/a')[0]
 		link = urljoin(videoURL, videoLinkElement.xpath('./@href')[0])
 		title = videoLinkElement.xpath('./text()')[0]
-		# print(link, title)
+
 		videoMetaInfo = videoItem.xpath('.//ul[contains(@class, "yt-lockup-meta-info")]')[0]
 		views = videoMetaInfo.xpath('./li[1]/text()')[0]
 		age = videoMetaInfo.xpath('./li[2]/text()')[0]
-		# print(views, age)
+
 		videos.append({ 'link': link, 'title': title, 'views': views, 'age': age })
 	return videos
 
-# print(channelVideos('user/TeamFortressTV/videos'))
