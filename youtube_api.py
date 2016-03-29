23cc36713fff6acbe84fea296977520008286c00
mirror <mirror12k@gmail.com>
2016-03-29 01:42:20 -0400

    - added parsing and scraping of the videos list

--- youtube_api.py
+++ youtube_api.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 
 import requests
+from urllib.parse import urljoin
+from lxml import html
 
 
+def channelVideos(channel):
+	videoURL = 'https://www.youtube.com/' + channel
+	res = requests.get(videoURL)
+	tree = html.fromstring(res.text)
+	videos = []
+	for videoItem in tree.xpath('//ul[@id = "channels-browse-content-grid"]/li'):
+		videoLinkElement = videoItem.xpath('.//*[contains(@class, "yt-lockup-title")]/a')[0]
+		link = urljoin(videoURL, videoLinkElement.xpath('./@href')[0])
+		title = videoLinkElement.xpath('./text()')[0]
+		# print(link, title)
+		videoMetaInfo = videoItem.xpath('.//ul[contains(@class, "yt-lockup-meta-info")]')[0]
+		views = videoMetaInfo.xpath('./li[1]/text()')[0]
+		age = videoMetaInfo.xpath('./li[2]/text()')[0]
+		# print(views, age)
+		videos.append({ 'link': link, 'title': title, 'views': views, 'age': age })
+	return videos
 
-res = requests.get('https://www.youtube.com/user/TeamFortressTV/videos')
-
-print (res.text)
+print(channelVideos('user/TeamFortressTV/videos'))
